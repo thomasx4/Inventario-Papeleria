00ae47feec1943cf57eaf26fb3dd29ab991293aa
juanmorenaso15 <juanjosemorenobenavides207@gmail.com>
2026-03-11 15:54:42 -0500

    feat: agrego archivo CategoriesServices y su metodo Create categories

--- src/main/java/com/inventario/papeleria/service/CategoriesService.java
+++ src/main/java/com/inventario/papeleria/service/CategoriesService.java
@@ -1,5 +1,33 @@
 package com.inventario.papeleria.service;
 
+import org.springframework.stereotype.Service;
+
+import com.inventario.papeleria.dto.CategoriesRequestDTO;
+import com.inventario.papeleria.dto.CategoriesResponseDTO;
+import com.inventario.papeleria.entity.Categories;
+import com.inventario.papeleria.repository.CategoriesRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
 public class CategoriesService {
-    
+    //----------------------------------------------------------------------------------------------
+    //Create categories
+
+    private final CategoriesRepository categoriesRepository;
+
+    public CategoriesResponseDTO createCategories(CategoriesRequestDTO categoriesRequestDTO){
+        Categories categories = new Categories();
+        categories.setName(categoriesRequestDTO.getName());
+        categories.setDescription(categoriesRequestDTO.getDescription());
+
+        categoriesRepository.save(categories);
+        CategoriesResponseDTO response = new CategoriesResponseDTO();
+        response.setId((categories.getId()));
+        response.setName(categories.getName());
+        response.setDescription(categories.getDescription());
+
+        return response;
+    }
 }
