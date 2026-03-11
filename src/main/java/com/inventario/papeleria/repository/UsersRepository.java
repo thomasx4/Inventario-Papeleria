4e386ff97339362322e1003bd9ee10687288c8d8
thomasx4 <castanedamunoz23@gmail.com>
2026-03-11 15:51:37 -0500

    feat: UsersRepository

--- src/main/java/com/inventario/papeleria/repository/UsersRepository.java
+++ src/main/java/com/inventario/papeleria/repository/UsersRepository.java
@@ -1,5 +1,23 @@
 package com.inventario.papeleria.repository;
 
-public class UsersRepository {
-    
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.inventario.papeleria.entity.User;
+
+@Repository
+public interface UsersRepository extends JpaRepository<User, Long>{
+
+
+Optional<User> findById(Long id);
+
+List<User> findByNameContaining(String name);
+
+Optional<User> findByEmail(String email);
+
+List<User> findByRole(String role);
+
 }
