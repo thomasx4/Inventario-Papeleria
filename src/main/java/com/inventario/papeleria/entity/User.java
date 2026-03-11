a7620e22d259f1f13d9f4f01993be6b3979cb619
thomasx4 <castanedamunoz23@gmail.com>
2026-03-11 15:27:45 -0500

    feat: User.java Entity

--- src/main/java/com/inventario/papeleria/entity/User.java
+++ src/main/java/com/inventario/papeleria/entity/User.java
@@ -1,5 +1,42 @@
 package com.inventario.papeleria.entity;
 
-public class User {
-    
-}
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.Table;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+
+import lombok.Data;
+
+    @Entity
+    @Table(name = "users")
+    @Data
+    public class User {
+
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        private Long id;
+
+        @Column(name = "name")
+        private String name;
+
+        @Column(name = "email")
+        private String email;
+
+        @Column(name = "password")
+        private String password;
+
+        @Column(name = "role")
+        private String role;
+
+        @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+        private List<Movement> movements;
+
+    }
+
